601ce40187319ab4ee73796757aa9258341a8c63
Kristian Tsaklev <ktsaklev@gmail.com>
2014-12-29 22:20:29 +0200

    Deleted everything

--- taffybar/test/test.hs
+++
@@ -1,5 +0,0 @@
-import XMonad.Core
-import Control.Monad.Reader
-
-extract :: (MonadReader XConf m) => m a -> a
-extract MonadReader d = d
--- zsh/zsh.sh
+++
@@ -1,4 +0,0 @@
-if [[ $0 =~ "zsh" ]]
-then
-	. /etc/zsh/zshrc
-fi
